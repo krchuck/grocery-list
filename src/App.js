01a73e4072fa7d334d4664b1a4e3a71a260a9ce5
Kenneth Chuck <krchuck@outlook.com>
2019-12-02 20:56:09 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react'
+import List from './List'
+import ItemForm from './ItemForm'
+import Footer from './Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class App extends React.Component {
+  state = { items: [], filter: 'All' }
+  
+  visibleItems = () => {
+    const { items, filter } = this.state
+    switch(filter) {
+      case 'Active':
+        return items.filter( t => !t.complete )
+      case 'Complete':
+        return items.filter( t=> t.complete )
+      default:
+        return items;
+    }
+  }
+  setFilter = (filter) => {
+    this.setState({ filter })
+  }
+
+  getUniqId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1)
+  }
+
+  handleClick = (id) => {
+    const {items} = this.state;
+    this.setState({
+      items: items.map( item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            complete: !item.complete
+          }
+        }
+        return item
+      })
+    })
+  }
+
+  addItem = (name) => {
+    const { items } = this.state;
+    const item = { name, id: this.getUniqId() , complete: false}
+    this.setState({ items: [item, ...items]
+    })
+  }
+  
+  render() {
+    const { items, filter } = this.state;
+    return (
+      <div>
+        <ItemForm addItem={this.addItem}/>
+        <List name="Item list" items={this.visibleItems()} itemClick={this.handleClick}
+         />
+         <Footer filter={filter} setFilter={this.setFilter} />
+      </div>
+    );
+    }
+  }
+
+  export default App;
---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const styles = {
+  link: {
+    cursor: 'pointer',
+    textDecoration: 'underline',
+    color: 'blue',
+    marginLeft: '5px',
+    marginRight: '5px',
+
+  }
+}
+
+const filterLink = (current, name, setFilter) => {
+  if (current === name)
+  return <span>{name}</span>
+  else
+  return <span sstyle={styles.link} onClick={() => setFilter(name) }>{name}</span>
+}
+
+const Footer = ({ filter, setFilter }) => (
+  <div>
+    { ['All', 'Active', 'Complete'].map( f=> filterLink(filter, f, setFilter))}
+  </div>
+)
+
+export default Footer
